46fe02cd7bccfaf8473442bc2097b5f3758b6e0f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-04-02 18:57:03 -0400

    I'm not sure I got the build time correct -- beind dependent on browser connect is probably not good. But also relying on transformIndexHTML is also flawed

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # vite-plugin-buildtime
+
+## Install
+
+
+```bash
+npm install --save-dev vite-plugin-buildtime
+```
+
+## Usage
+
+in your vite.config.js (mjs,ts,mts,etc)
+```js
+import { buildTime } from 'vite-plugin-buildtime';
+
+// ...
+plugins: [
+    buildTime((data) => {
+        console.log(data);
+    });
+]
+
+```
---
+++ src/index.js
@@ -0,0 +1,106 @@
+
+/**
+ * NOTES:
+ *
+ * On boot
+ * 1. configResolved
+ * 2. options
+ * 3. configureServer
+ * 4. buildStart
+ * 5. handleHotUpdate
+ *
+ * On config file change (this file)
+ * -5. configResolved
+ * -4. options
+ * -3. configureServer
+ * -2. buildEnd
+ * -1. closeBundle
+ * 1. buildStart
+ * 2. handleHotUpdate
+ *
+ * Whenever a browser tab is active
+ * 1. transformIndexHTML (per HTML entry point (app, tests))
+ *
+ * Whenever a file is saved with no changes
+ * 0. nothing -- however, if a browser tab is open, the above still holds true
+ *
+ * Whenever a file is saved with changes
+ * 1. handleHotUpdate
+ * 2. transformIndexHTML
+ */
+
+/**
+ * Tracks the build time
+ */
+export function buildTime(callback) {
+
+  if (!callback) {
+    throw new Error(`A callback must be passed to the buildTime plugin. This callback will receive buildtime information`);
+  }
+
+  let lastStart;
+  let finishedBoot = false;
+  let lastUpdate;
+
+
+  return {
+    /**
+     * Info about this plugin
+     */
+    name: 'vite-plugin-buildtime',
+
+
+    /**
+    * Vite plugin apis
+    */
+    hotUpdate() {
+      lastUpdate = Date.now();
+    },
+    buildStart() {
+      lastStart = Date.now();
+      finishedBoot = false;
+    },
+
+    /**
+     * Called each time an HTML entrypoint is output
+     */
+    transformIndexHTML() {
+      if (lastStart) {
+        if (!finishedBoot) {
+          finishedBoot = true;
+          let duration = Date.now() - lastStart;
+          /**
+           * We have to set to null here because the time between HTML entrypoints is very close
+           */
+          lastStart = null;
+
+          callback({
+            duration,
+            type: 'initial-build',
+          });
+        }
+      }
+
+      if (lastUpdate) {
+        let duration = Date.now() - lastUpdate;
+        lastUpdate = null;
+
+        callback({
+          duration,
+          type: 'rebuild',
+        });
+      }
+    },
+    /**
+    * NOTE: we can't use configureServer's 
+    *     server.ws.on('connection'
+    *     because the connection event doesn't fire if the tab isn't open.
+    *     which means the build time will be perpetually delayed until the brower is focused
+    *     (Chrome will aggressively deactivate tabs not in focus, for example)
+    */
+    // configureServer(server) {
+    //   server.ws.on('connection', () => {
+    //   });
+    // },
+  }
+}
